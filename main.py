e30c98b124b6186019185e52268d3331262beaec
ardayasar <erddoerdo12@gmail.com>
2022-11-28 11:37:17 +0300

    Activate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from networkJobs import networkJobs
 from fileJobs import fileJobs
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from threading import Thread
 import time
 import random
@@ -8,14 +8,14 @@ import os
 import dotenv
 import subprocess
 
-load_dotenv(dotenv_path='../../.env')
+dotenv.load_dotenv(dotenv_path='../../.env')
 
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 gpio_pins = [5, 19, 16, 23, 25, 27]
-#
-# for pin in gpio_pins:
-#     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+for pin in gpio_pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 config = {
     'deviceID': os.getenv("DEVICEID"),
@@ -95,23 +95,23 @@ while True:
     try:
         if config['isActive'] and config['isRegistered']:
             pinStatus = []
-            # for pin in range(config['buttonCount']):
-            #     pinStatus.append(False if int(GPIO.input(gpio_pins[pin])) == 0 else True)
-            # for pin_slot in range(len(pinStatus)):
-            #     if pinStatus[pin_slot]:
-            #         # Print
-            #         filePath = config['fileLocation'].replace(".", "") + str(pin_slot+1) + "/"
-            #         pathFiles = FJ.getFiles(pin_slot+1)
-            #         pathLen = len(pathFiles)
-            #         selectedFile = random.randint(1, pathLen)
-            #         selectedFile_name = pathFiles[selectedFile-1]
-            #         selectedFile_path = os.getcwd() + filePath + selectedFile_name
-            #         # subprocess.run(["lp", selectedFile_path + '.pdf'], capture_output=True)
-            #         print('Printing -> ' + selectedFile_path)
-                    # os.environ['printCount'] = str(int(os.getenv('printCount')) + 1)
-                    # dotenv.set_key('../../.env', "VERSION", os.environ["printCount"])
-            #         time.sleep(3)
-            #         continue
+            for pin in range(config['buttonCount']):
+                pinStatus.append(False if int(GPIO.input(gpio_pins[pin])) == 0 else True)
+            for pin_slot in range(len(pinStatus)):
+                if pinStatus[pin_slot]:
+                    # Print
+                    filePath = config['fileLocation'].replace(".", "") + str(pin_slot+1) + "/"
+                    pathFiles = FJ.getFiles(pin_slot+1)
+                    pathLen = len(pathFiles)
+                    selectedFile = random.randint(1, pathLen)
+                    selectedFile_name = pathFiles[selectedFile-1]
+                    selectedFile_path = os.getcwd() + filePath + selectedFile_name
+                    # subprocess.run(["lp", selectedFile_path + '.pdf'], capture_output=True)
+                    print('Printing -> ' + selectedFile_path)
+                    os.environ['printCount'] = str(int(os.getenv('printCount')) + 1)
+                    dotenv.set_key('../../.env', "VERSION", os.environ["printCount"])
+                    time.sleep(3)
+                    continue
 
             print(pinStatus)
             time.sleep(1)
