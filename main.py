d761209cb1c6f97e1e04c35ad56fbbe50143ad7a
Arda Yasar <erddoerdo12@gmail.com>
2024-11-03 23:23:50 +0300

    NEW V1.03 UPDATE

--- main.py
+++ main.py
@@ -1,256 +1,68 @@
-import datetime
-from networkJobs import networkJobs
-from fileJobs import fileJobs
-# import RPi.GPIO as GPIO
-# import keyboard
-from pynput.keyboard import Listener
-from threading import Thread
+from statistics import statistics
+from network import networkJobs
+from sync import device_sync
+from fs_jobs import fileJobs
+from printer import Printer
+from buttons import buttons
+from datetime import datetime
+from dotenv import load_dotenv, find_dotenv
+from anydesk import anydesk
+from env_controller import set_env_variable, reload_env
+
 import time
 import os
-import dotenv
-import subprocess
-import random
-from anydesk import anydesk
-from printerDrivers import printerDriver
 
+load_dotenv()
+
+device_id = os.getenv('DEVICEID')
+authKey = os.getenv('DEVICEKEY')
+printer_model = os.getenv('PRINTER_MODEL')
+
+network = networkJobs(device_id, authKey)
+statistics = statistics()
+printer = Printer(printer_model, statistics)
+syncing = device_sync(network, printer)
+filecontroller = fileJobs()
+buttoncontroller = buttons(filecontroller, printer)
 adk = anydesk()
-
-dotenv.load_dotenv(dotenv_path='home/pi/Desktop/BHS-Upgrader/.env')
-
-keyboard_pins = ["1", "2", "3", "4", "5", "6"]
-
-config = {
-    'deviceID': os.getenv("DEVICEID"),
-    'authKey': os.getenv("AUTHKEY"),
-    'isRegistered': False,
-    'fileLocation': os.getcwd() + "/versions/" + str(os.getenv('VERSION')) + "/pdf/",
-    'isActive': False,
-    'buttonCount': 0,
-    'isBetweenTime': False,
-    'deviceStartTime': datetime.time(9, 0),
-    'deviceEndTime': datetime.time(17, 0),
-    'device_second_StartTime': datetime.time(1, 0),
-    'device_second_EndTime': datetime.time(8, 0),
-    'isDelayAvailable': False,
-    'printer_information': ""
-}
-
-
-def asyncDevice():
-    while True:
-        try:
-            NJ = networkJobs(config['deviceID'], config['authKey'])
-            config['isRegistered'] = NJ.isRegistered()
-            config['isActive'] = NJ.isActive()
-            bc = NJ.getButtonCount()
-            if bc > 0:
-                config['buttonCount'] = bc
-            else:
-                quit("Button count problem")
-
-            FJ = fileJobs(config['buttonCount'], config['fileLocation'])
-            FJ.generateFolders()
-
-            fileList = []
-
-            for folder in range(config['buttonCount']):
-                folder += 1
-                result = FJ.getFiles(folder)
-                fileList.append(result)
-
-            jobList = NJ.asyncFiles(fileList)
-            print(jobList)
-            for folder in range(len(jobList)):
-                FJ.deleteFiles(folder+1, jobList[folder][0])
-                for file in jobList[folder][1]:
-                    content = NJ.downloadFile(file)
-                    if not content:
-                        continue
-                    FJ.saveFile(folder+1, file, content)
-
-            for folder in range(config['buttonCount']):
-                path = config['fileLocation'] + str(folder+1)
-                files = os.listdir(path)
-                for file in files:
-                    lpath = config['fileLocation'] + str(folder + 1)
-                    lpath += "/" + file
-                    if os.path.getsize(lpath) < 10:
-                        os.remove(lpath)
-
-            # print(jobList)
-            working_times = NJ.getWorkingTimes()
-            config['deviceStartTime'] = working_times[0][0]
-            config['deviceEndTime'] = working_times[0][1]
-            config['device_second_StartTime'] = working_times[1][0]
-            config['device_second_EndTime'] = working_times[1][1]
-
-            time.sleep(300)
-        except Exception as e:
-            print(e)
-            time.sleep(10)
-
-
-def checkPrinter():
-    try:
-        NJ = networkJobs(config['deviceID'], config['authKey'])
-        printer_information = NJ.getPrinterInformation()
-        printer_driver = printerDriver(printer_information['printerName'], printer_information['printerManufacturer'], printer_information['printerID'])
-        last_call = ""
-        while True:
-            try:
-                printer_status, level_code = printer_driver.getStatus()
-                if printer_status != last_call:
-                    last_call = printer_status
-                    NJ.updatePrinterStatus(printer_status, level_code)
-                time.sleep(360)
-            except Exception as e:
-                print(e)
-                time.sleep(360)
-    except Exception as e:
-        while True:
-            print("Printer is offline. To reactivate, please connect to wifi and restart device")
-            time.sleep(360)
-
-
-NJ = networkJobs(config['deviceID'], config['authKey'])
-
-if not NJ.isRegistered():
-    if NJ.setup():
-        print("Setup request has been sent to server!")
-    else:
-        quit("There was a problem while setting up the device!")
-else:
-    config['isRegistered'] = True
 
 if os.getenv("ANYDESK") == "0":
     try:
         if adk.generateId():
             time.sleep(10)
-            if adk.setPassword("fattoli3417"):
+            if adk.setPassword(os.getenv("PASSWORD")):
                 time.sleep(4)
                 anyID = adk.getId()
-                if NJ.updateAnyDeskInfo(anyID, os.getenv("PASSWORD")):
-                    # dotenv.set_key("../../.env", "ANYDESK", anyID)
+                if network.updateAnyDeskInfo(anyID, os.getenv("PASSWORD")):
+                    set_env_variable("ANYDESK", '1')
+                    reload_env(find_dotenv())
                     print("Setup completed")
     except Exception as e:
         print(e, 'anydesk')
 
-while not config["isRegistered"]:
-    if NJ.isRegistered():
-        print("Verified")
-        config['isRegistered'] = True
-        bc = NJ.getButtonCount()
-        if bc > 0:
-            config['buttonCount'] = bc
-        else:
-            quit("Button count problem")
-    else:
-        time.sleep(10)
-
-
-deviceAsync = Thread(target=asyncDevice)
-deviceAsync.start()
-
-printerStatus = Thread(target=checkPrinter)
-printerStatus.start()
-
-FJ = fileJobs(config['buttonCount'], config['fileLocation'])
-
-
-def controlKey(input_key):
-    try:
-        input_key = int(str(input_key).replace("'", ''))
-    except Exception as e:
-        lsp = []
-        for i in range(config['buttonCount']):
-            lsp.append(False)
-        # print(e)
-        return lsp
-
-    lsl = []
-    for i in range(config['buttonCount']):
-        apData = False
-        if i+1 == input_key:
-            apData = True
-
-        lsl.append(apData)
-
-    return lsl
-
-
-def onButtonPress(key):
-    # print(key)
-    if str(key).replace("'", '') == 'q':
-        quit()
-
-
-def onButtonRelease(pushedButton, listener):
-    try:
-
-        if config['isDelayAvailable'] == True:
-            return
-
-        if config['isActive'] and config['isRegistered']:
-            pinStatus = controlKey(pushedButton)
-
-            if True in pinStatus:
-                config['isDelayAvailable'] = True
-                for pin_slot in range(len(pinStatus)):
-                    if pinStatus[pin_slot]:
-                        filePath = config['fileLocation'] + str(pin_slot+1) + "/"
-                        pathFiles = FJ.getFiles(pin_slot+1)
-                        pathLen = len(pathFiles)
-                        if pathLen > 0:
-                            selectedFile = random.randint(1, pathLen)
-                            selectedFile_name = pathFiles[selectedFile-1]
-                            selectedFile_path = filePath + selectedFile_name
-                            subprocess.run(['cancel', '-a'])
-                            subprocess.run(["lp", "-o fit-to-page", selectedFile_path + '.pdf'], capture_output=True)
-                            print('Printing -> ' + selectedFile_path)
-                            os.environ['printCount'] = str(int(os.getenv('printCount')) + 1)
-                            # dotenv.set_key('home/pi/Desktop/BHS-Upgrader/.env', "printCount", os.environ["printCount"])
-                            listener.stop()
-                            # listener.join()
-                            time.sleep(5)
-                            config['isDelayAvailable'] = False
-                            run_listener()
-                            continue
-                        else:
-                            print("Selected buttons file is empty. Please assign a category or add story to category")
-                            time.sleep(3)
-                            config['isDelayAvailable'] = False
-
-            # print(pinStatus)
-            time.sleep(0.4)
-    except Exception as e:
-        print(e)
-        print("Error")
-
-def is_time_in_range(start, end):
-    current_time = datetime.datetime.now().time()
-    if start <= end:
-        return start <= current_time <= end
-    else:
-        return start <= current_time or current_time <= end
-
-
-def run_listener():
-    listener = Listener(on_press=None, on_release=lambda key: onButtonRelease(key, listener))
-    listener.start()
-    return listener
-
-
-listener = run_listener()
-
 while True:
-    if is_time_in_range(config['deviceStartTime'], config['deviceEndTime']) or is_time_in_range(config['device_second_StartTime'], config['device_second_EndTime']):
-        if listener is None or not listener.is_alive():
-            listener = run_listener()
-    else:
-        try:
-            listener.stop()
-            # listener.join()
-            listener = None
-        except Exception as e:
-            print(e)
-    time.sleep(30)
+    try:
+        syncing.syncing_status = network.is_connected()
+
+        deviceStartTime = syncing.deviceStartTime
+        deviceEndTime = syncing.deviceEndTime
+        device_second_StartTime = syncing.device_second_StartTime
+        device_second_EndTime = syncing.device_second_EndTime
+
+        current_time = datetime.now().time()
+
+        if (deviceStartTime <= current_time <= deviceEndTime) or (device_second_StartTime <= current_time <= device_second_EndTime):
+            if syncing.isActive and syncing.isRegistered:
+                buttoncontroller.is_listening_time = True
+                buttoncontroller.is_device_active = True
+            else:
+                buttoncontroller.is_listening_time = False
+                buttoncontroller.is_device_active = False
+        else:
+            buttoncontroller.is_listening_time = False
+
+        time.sleep(30)
+    except Exception as e:
+        print('----------[GRANT ERROR]----------')
+        print(e)
+        time.sleep(30)
